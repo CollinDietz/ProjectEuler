fbcdde8e8dd78e6eeccb38795e7d8950e3c9ac52
Collin Dietz <c4dietz@gmail.com>
2016-09-26 14:00:57 -0400

    Update Prob20FactorialDigitSum.py

--- Prob20FactorialDigitSum.py
+++ Prob20FactorialDigitSum.py
@@ -1,3 +1,12 @@
+#n! means n × (n − 1) × ... × 3 × 2 × 1
+#
+#For example, 10! = 10 × 9 × ... × 3 × 2 × 1 = 3628800,
+#and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
+#
+#Find the sum of the digits in the number 100!
+#
+#https://projecteuler.net/problem=20
+
 def facDigitSum(n):
     # determines the sum of the digits of n!
     factorial = 1  # holds the value of n!

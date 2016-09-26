67be7472b9f778c33a7b21927153acf2b699e625
Collin Dietz <c4dietz@gmail.com>
2016-09-26 13:14:20 -0400

    Update namescores.cpp

--- Prob22NameScores/namescores.cpp
+++ Prob22NameScores/namescores.cpp
@@ -16,6 +16,8 @@ which is worth 3 + 15 + 12 + 9 + 14 = 53, is the 938th name in the list.
 So, COLIN would obtain a score of 938 × 53 = 49714.
 
 What is the total of all the name scores in the file?
+
+https://projecteuler.net/problem=22
 */
 
 using namespace std;
